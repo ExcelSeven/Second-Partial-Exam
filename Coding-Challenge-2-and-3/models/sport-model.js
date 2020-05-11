be1f742673ef31232436dfcf8e21173ce35b4660
Eli Moreira <coincidir777@hotmail.com>
2020-05-11 08:28:52 -0500

    Updated

--- Coding-Challenge-2-and-3/models/sport-model.js
+++ Coding-Challenge-2-and-3/models/sport-model.js
@@ -1,6 +1,42 @@
 const mongoose = require( 'mongoose' );
+const bodyParser = require( 'body-parser' );
 
-/* Your code goes here */
+const jsonParser = bodyParser.json();
+
+let sportList = [] 
+
+app.post( '/sports/addSport/:sportId', ( req, res ) => {
+    console.log( req.params );
+
+    let id = req.params.id;
+    let name = req.params.name;
+    let num_players = req.params.num_players;
+
+    if (!sport) {
+        res.statusMessage = "Parameters do not match a sport."
+        return res.status (409).end();
+    }
+
+    let check = true
+
+    for( let i = 0; i < .length; i++) {
+        if(sportList.id == id) {
+            check = !check;
+            break;
+        }
+    }
+
+    if (check) {
+        let newSport = {id, name, num_players};
+        sportList.push(newSport);
+
+        return res.status(201).json(newSport);
+    
+    else {
+        res.statusMessage = "ID already belongs to another sport"
+        return res.status (400).end();
+    } 
+}
 
 module.exports = {
     
